49072d2bb2cf67844d3300303f0dbf01b37484cd
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2021-11-10 22:01:22 +0530

    responsiveness added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const hamburger = document.getElementById('hamburger');
+const nav_bg = document.getElementById('nav_bg');
+const nav_links = document.getElementById('nav_links');
+
 document.querySelectorAll('a[href^="#btn"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -6,4 +10,10 @@ document.querySelectorAll('a[href^="#btn"]').forEach(anchor => {
             behavior: 'smooth'
         });
     });
+});
+
+hamburger.addEventListener("click", () => {
+    nav_bg.classList.toggle("open");
+    nav_links.classList.toggle("open");
+    hamburger.classList.toggle("open");
 });
