0cada222d264cfcf0d72026ea710e24629eb2357
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2021-11-10 19:04:02 +0530

    nav bar and footer added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href^="#btn"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
