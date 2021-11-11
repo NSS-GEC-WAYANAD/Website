4182cef00e127ea2049492bfb7252ab03e43bc30
Basil Reji <84758142+basil-reji@users.noreply.github.com>
2021-11-11 17:03:07 +0530

    latest program section added

--- js/jq_script.js
+++ js/jq_script.js
@@ -1,3 +1,12 @@
+document.querySelectorAll('a[href^="#btn"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
 function isIntoView(elem)
 {
--- js/script.js
+++ js/script.js
@@ -2,16 +2,6 @@ const hamburger = document.getElementById('hamburger');
 const nav_bg = document.getElementById('nav_bg');
 const nav_links = document.getElementById('nav_links');
 
-document.querySelectorAll('a[href^="#btn"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
 hamburger.addEventListener("click", () => {
     nav_bg.classList.toggle("open");
     setTimeout(function() {
